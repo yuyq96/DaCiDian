921e38b686699b30d3c060dd76d9832e1c805900
Ya-Qi Yu <yuyq@lamda.nju.edu.cn>
2020-04-03 21:14:51 +0800

    Clean dict

--- DaCiDian.py
+++ DaCiDian.py
@@ -29,8 +29,8 @@ for l in open(word_to_syllable_file): # "15	YI_1 WU_3;YAO_1 WU_3"
 	for pron in prons:
 		phone_seq = []
 		for syllable in pron.split():
-			base,tone = syllable.split('_')
+			base,tone = syllable[:-2], syllable[-1]
 			phones = [phn for phn in syllable_to_phones[base]]
-			phones[-1] = phones[-1]+'_'+tone
+			phones[-1] = phones[-1]+tone
 			phone_seq.extend(phones)
 		sys.stdout.write(word + '\t' + ' '.join(phone_seq) + '\n')
